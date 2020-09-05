b14347e3c344d322e59b0600bd0b88303a9bd581
Phathdt <phathdt379@gmail.com>
2020-09-05 22:35:14 +0700

    add step

--- vars/checkoutBranch.groovy
+++ vars/checkoutBranch.groovy
@@ -2,6 +2,13 @@ def call(branch) {
   pipeline {
     agent any
     stages {
+      stage('pull code') {
+        steps {
+          echo 'ls -l'
+          echo 'pwd'
+        }
+      }
+
       stage('Checkout branch') {
         steps {
           echo "Hello, ${branch}."
