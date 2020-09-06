2fbc3dd149fdf3b928fe0dec47c78c35b1895a87
Phathdt <phathdt379@gmail.com>
2020-09-06 10:49:55 +0700

    update job

--- vars/checkoutBranch.groovy
+++ vars/checkoutBranch.groovy
@@ -1,25 +1,3 @@
-def call(String repo_url, String branch) {
-  pipeline {
-    agent any
-    stages {
-      stage('pull repo_url') {
-        steps {
-          echo repo_url
-
-          sh 'rm -rf source_code'
-          sh "git clone ${repo_url} source_code"
-        }
-      }
-      stage('Checkout branch') {
-        steps {
-          sh '''
-            cd source_code
-            git reset HEAD --hard
-            git fetch origin ${branch}
-            git checkout ${repo_url}
-          '''
-        }
-      }
-    }
-  }
+def call(String repo_url) {
+  return repo_url
 }
