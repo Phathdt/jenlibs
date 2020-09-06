398d5e15443d37a1a63f7e967a596059fbfde22f
Phathdt <phathdt379@gmail.com>
2020-09-06 10:11:00 +0700

    add step

--- vars/checkoutBranch.groovy
+++ vars/checkoutBranch.groovy
@@ -1,10 +1,23 @@
-def call(branch) {
+def call(repo_url, branch) {
   pipeline {
     agent any
     stages {
+      stage('pull repo_url') {
+        steps {
+          sh '''
+            rm -rf source_code
+            git clone ${repo_url} source_code
+          '''
+        }
+      }
       stage('Checkout branch') {
         steps {
-          sh "cd /var/lib/jenkins/source_code && git checkout ${branch}"
+          sh '''
+            cd source_code
+            git reset HEAD --hard
+            git fetch origin ${branch}
+            git checkout ${repo_url}
+          '''
         }
       }
     }
