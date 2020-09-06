22518da88a3dd7ced2934f5bd20e773444c3551e
Phathdt <phathdt379@gmail.com>
2020-09-06 11:17:49 +0700

    update job

--- vars/sendEmail.groovy
+++ vars/sendEmail.groovy
@@ -1,3 +1,6 @@
+import hudson.model.Result
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+
 def call(RunWrapper currentBuild, List<String> emailList) {
     if (!emailList) {
         return
